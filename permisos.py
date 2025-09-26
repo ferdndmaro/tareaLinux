0bad4d5fbe8091044dccdb035c4759cc92348a36
Fernando Madera <ferddmaro@gmail.com>
2025-09-26 11:49:29 -0600

    primera snapshot

---
+++ permisos.py
@@ -0,0 +1,48 @@
+def separador(permisosUser,permisosGroup,permisosOther):
+    for l in permisosUser:
+        letrasUser = [letra for letra in permisosUser]
+        print(letrasUser)
+    for m in permisosGroup:
+        letrasGroup = [letra for letra in permisosGroup]
+        print(letrasGroup)
+    for n in permisosOther:
+        letrasOther = [letra for letra in permisosOther]
+        print(letrasOther)
+    return letrasUser,letrasGroup,letrasOther
+
+
+
+def main():
+    print("|       rwxrwxrwx |chmod_mate| rwxrwxrwx     |\n|cambiar permisos de archivos y directorios:)|")
+    print("-____________________________________________-")
+    print("Valores de entrada permitidos: (rwx o ---)")
+    permisosUser = str(input("Introduzca los permisos de usuario (owner): ")).lower()
+    if len(permisosUser) != 3:
+        noValido = True
+        while noValido:
+            print("Error, se espera una cadena de 3 caracteres.")
+            permisosUser = str(input("Introduzca los permisos de usuario (owner): ")).lower()
+            if len(permisosUser) == 3:
+                noValido = False
+
+    permisosGroup = str(input("Introduzca los permisos de grupo (group): ")).lower()
+    if len(permisosGroup) != 3:
+        noValido = True
+        while noValido:
+            print("Error, se espera una cadena de 3 caracteres.")
+            permisosGroup = str(input("Introduzca los permisos de grupo (group): ")).lower()
+            if len(permisosGroup) == 3:
+                noValido = False
+    permisosOther = str(input("Introduzca los permisos de otros (others): ")).lower()
+    if len(permisosOther) != 3:
+        noValido = True
+        while noValido:
+            print("Error, se espera una cadena de 3 caracteres.")
+            permisosOther = str(input("Introduzca los permisos de otros (others):" )).lower()
+            if len(permisosOther) == 3:
+                noValido = False
+    return permisosUser,permisosGroup,permisosOther
+
+main()
+
+
